0a8fef224dee942451b8ee89158f89f41adedf6b
kunal2503 <kunaldeshmukh07d@email.com>
2025-05-07 13:08:21 +0530

    Inital commit

---
+++ backend/index.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cartRoutes = require("./routes/cart");
+const productRoutes = require("./routes/productRoute");
+const orderRoutes = require("./routes/orderRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+const { default: orderModel } = require('./models/orderModel');
+
+
+
+const app = express();
+
+// Middleware
+app.use(cors({
+    origin: "http://localhost:5173", // Replace with your frontend URL
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/products", productRoutes)
+app.use("/api/orders",orderRoutes )
+app.use("/api/users/profile", profileRoutes);
+
+// Connect MongoDB
+// "mongodb://localhost:27017/smart_Buy"
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.error(err));
+
+
+
+// Default route
+app.get("/", (req, res) => {
+    res.send("Hello from the server");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
---
+++ frontend/Smart-Buy/src/pages/ChatBot.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatBot = () => {
+  return (
+    <div className='flex justify-center items-center font-bold text-2xl'>
+        feature building phase comming soon..
+    </div>
+  )
+}
+
+export default ChatBot
